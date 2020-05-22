d3c744915a07fdc05c02331982f9b1a1e23df969
pankaj.koti <pankaj.koti@olx.com>
2020-05-22 13:59:47 +0200

    Add docstring to functions in python files

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,37 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+
+
+
+"""
+    Queries to clean the database by dropping if tables exists
+    :param cur: cursor object to execute queries
+    :param conn: pyscopg2 connection object to Redshift cluster
+    :return: None
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    DDL queries to be run to create staging tables, as well as fact and dimension tables
+    :param cur: cursor object to execute queries
+    :param conn: pyscopg2 connection object to Redshift cluster
+    :return: None
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Orchestrator function responsible to call drop and create table functions
+    :return: None
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- etl.py
+++ etl.py
@@ -4,18 +4,34 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    First step of ETL copying data from files in S3 bucket to staging tables in Redshigt
+    :param cur: cursor object to execute queries
+    :param conn: pyscopg2 connection object to Redshift cluster
+    :return: None
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Insert data from staging tables in Redshift to modelled fact and dimension tables within the same Redshift cluster
+    :param cur: cursor object to execute queries
+    :param conn: pyscopg2 connection object to Redshift cluster
+    :return: None
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Orchestrator function to call function load data to staging and then into modelled tables
+    :return: None
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
